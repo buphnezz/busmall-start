7ad831b0e5fa0ae238cf55e596bbf22405c89486
zachary schumpert <buphnezz@gmail.com>
2018-01-22 13:38:12 -0800

    added pseudocode

---
+++ ap.js
@@ -0,0 +1,10 @@
+'use strict';
+//display 3 images at a time, side by side by side
+// manage the size and position of the images
+// users should be able to select an image
+// I want to track which image is selected
+// track how many times each image was selected
+// display the number of times each image was selected
+// user must click 25 times before results are displayed
+// calculate and display the percentage of times an image was clicked when it was displayed.
+// make it pretty.
