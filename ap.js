36f696c4a53994e9c189ea7653adb8be18440e42
zachary schumpert <buphnezz@gmail.com>
2018-01-22 14:32:35 -0800

    added all images to html

--- ap.js
+++ ap.js
@@ -1,5 +1,7 @@
 'use strict';
-//display 3 images at a time, side by side by side
+
+//display 3 images at a time 
+// I want those 3 images to display side by side by side
 // manage the size and position of the images
 // users should be able to select an image
 // I want to track which image is selected
@@ -8,3 +10,5 @@
 // user must click 25 times before results are displayed
 // calculate and display the percentage of times an image was clicked when it was displayed.
 // make it pretty.
+
+
